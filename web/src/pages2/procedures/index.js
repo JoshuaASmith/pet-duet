193aaf2cdfb3306261a6bbc168044c3e3298eb7c
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-12-15 14:33:48 -0500

    added procedures filter search

--- web/src/pages2/procedures/index.js
+++ web/src/pages2/procedures/index.js
@@ -7,18 +7,20 @@ const {filter, propEq} = require('ramda')
 
 const Procedures = React.createClass({
     getInitialState() {
-        return {procedures: []}
+        return {procedures: [], filtered: []}
     },
     componentDidMount() {
-        data.list('procedures').then(procedures => {
-            console.log('test', procedures)
-            return procedures
-        }).then(procedures => procedures = procedures.docs).then(filter(propEq('parent_id', this.props.petID))).then(procedures => {
-            this.setState({procedures: procedures})
+        data.list('procedures').then(procedures => procedures = procedures.docs).then(filter(propEq('parent_id', this.props.petID))).then(procedures => {
+            this.setState({procedures: procedures, filtered: procedures})
+        })
+    },
+    filter(e) {
+        this.setState({
+            filtered: filter(procedure => procedure.procedure.indexOf(e.target.value) === 0, this.state.procedures)
         })
     },
     render() {
-        const li = procedure => <li key={procedure._id} className="ph3 pv2 bb b--light-silver tc avenir black hover-light-red">
+        const li = procedure => <li key={procedure._id} className="ph2 pv2 bb b--light-silver tc avenir black hover-light-red">
             <Link className="link black hover-light-red avenir grow" to={`/procedures/${procedure._id}/show?parent_id=${this.props.petID}`}>{procedure.procedure}
                 , {procedure.datePerformed}</Link>
         </li>
@@ -29,9 +31,12 @@ const Procedures = React.createClass({
                 </div>
                 <Break/>
                 <div className="pv2">
+                    <div className="mt3 w-25 center">
+                        <input className="pa2 input-reset ba b--dark-blue bg-transparent hover-bg-black hover-white w-100 tc" type="text" name="pet-search" onChange={this.filter}/>
+                    </div>
                     <div>
                         <ul className="list pl0 ml0 center mw6 ba b--light-silver br3 bg-light-gray black">
-                            {this.state.procedures.map(li)}
+                            {this.state.filtered.map(li)}
                         </ul>
                     </div>
                 </div>
